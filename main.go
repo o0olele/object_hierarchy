7c203fba5e8dfd5f6146e27b3d999ec4b4ae5462
o0olele <43069191+o0olele@users.noreply.github.com>
2023-12-17 15:15:32 +0800

    【code】update main

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"scene_graph/obj"
 	"time"
 
 	"github.com/g3n/engine/app"
@@ -46,24 +47,25 @@ func main() {
 	a.Subscribe(window.OnWindowSize, onResize)
 	onResize("", nil)
 
-	// Create a blue torus and add it to the scene
-	geom := geometry.NewTorus(1, .4, 12, 32, math32.Pi*2)
-	mat := material.NewStandard(math32.NewColor("DarkBlue"))
-	mesh := graphic.NewMesh(geom, mat)
-	scene.Add(mesh)
+	// Create a blue Cube
+	mesh1 := graphic.NewMesh(
+		geometry.NewCube(1),
+		material.NewStandard(math32.NewColor("DarkBlue")),
+	)
+	mesh1.SetPosition(0, 0, 0)
+	scene.Add(mesh1)
 
-	// Create and add a button to the scene
-	btn := gui.NewButton("Make Red")
-	btn.SetPosition(100, 40)
-	btn.SetSize(40, 40)
-	btn.Subscribe(gui.OnClick, func(name string, ev interface{}) {
-		mat.SetColor(math32.NewColor("DarkRed"))
-	})
-	scene.Add(btn)
+	// Create a red Cube
+	mesh2 := graphic.NewMesh(
+		geometry.NewCube(0.5),
+		material.NewStandard(math32.NewColor("DarkRed")),
+	)
+	mesh2.SetPosition(1.5, 0, 0)
+	scene.Add(mesh2)
 
 	// Create and add lights to the scene
-	scene.Add(light.NewAmbient(&math32.Color{1.0, 1.0, 1.0}, 0.8))
-	pointLight := light.NewPoint(&math32.Color{1, 1, 1}, 5.0)
+	scene.Add(light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.8))
+	pointLight := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 5.0)
 	pointLight.SetPosition(1, 0, 2)
 	scene.Add(pointLight)
 
@@ -73,9 +75,34 @@ func main() {
 	// Set background color to gray
 	a.Gls().ClearColor(0.5, 0.5, 0.5, 1.0)
 
+	objScene := obj.NewScene()
+
+	obj1 := objScene.NewObject()
+	mesh1.WorldPosition(&obj1.Pos)
+	objScene.Add(obj1)
+
+	obj2 := objScene.NewObject()
+	mesh2.WorldPosition(&obj2.Pos)
+	obj1.Add(obj2)
+
+	objScene.UpdateMatrixWorld(false)
+
 	// Run the application
 	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
 		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
+
+		obj1.RotateY(float32(deltaTime.Seconds()))
+		obj2.RotateZ(float32(deltaTime.Seconds()))
+
+		objScene.UpdateMatrixWorld(false)
+
+		// Update mesh position
+		mesh1.SetPositionVec(obj1.GetWorldPosition())
+		mesh2.SetPositionVec(obj2.GetWorldPosition())
+		// Update mesh rotation
+		mesh1.SetRotationQuat(obj1.GetWorldQuaternion())
+		mesh2.SetRotationQuat(obj2.GetWorldQuaternion())
+
 		renderer.Render(scene, cam)
 	})
 }
